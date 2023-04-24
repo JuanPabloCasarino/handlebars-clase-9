a04b8fafa484c3a4b9645ab8d297ba95407612bf
d3xb0t <d3xb0t@gmail.com>
2023-04-24 09:44:46 -0500

    Agregados estilos dinamicos

--- routes/handlers.js
+++ routes/handlers.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (requests, response) => {
-    response.render('index')
+    response.render('index', {style: 'style.css'})
 })
 
 router.get('/about', (request, response) => {
-    response.render('about')
+    response.render('about', {style: 'about.css'})
 })
 
 module.exports = router
